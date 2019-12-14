47f33313769e6e031ddc016284c67991eb9988fd
Darina Toneva <tonevadarina@gmail.com>
2019-12-14 16:16:20 +0100

    HTML of Teaching material is done

--- js/script.js
+++ js/script.js
@@ -66,3 +66,26 @@ function clean_button_pressed_effect() {
 		drum_buttons[i].blur();
 	}
 }
+
+//SLIDER
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+	showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	if (n > x.length) {
+		slideIndex = 1
+	}
+	if (n < 1) {
+		slideIndex = x.length
+	}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[slideIndex - 1].style.display = "block";
+}
